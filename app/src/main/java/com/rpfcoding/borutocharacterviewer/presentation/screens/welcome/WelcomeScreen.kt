461c3795780828cc8edd9a721cd514bfcb6029e9
riley <rileyfarro@gmail.com>
2022-02-22 10:22:28 +0800

    Added extension variable for MaterialColor in Color class. Created Dimens class to create constants for dp. Created composable for PagerScreen composable function and its previews.

--- app/src/main/java/com/rpfcoding/borutocharacterviewer/presentation/screens/welcome/WelcomeScreen.kt
+++ app/src/main/java/com/rpfcoding/borutocharacterviewer/presentation/screens/welcome/WelcomeScreen.kt
@@ -1,18 +1,26 @@
 package com.rpfcoding.borutocharacterviewer.presentation.screens.welcome
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
 import com.google.accompanist.pager.rememberPagerState
+import com.rpfcoding.borutocharacterviewer.R
 import com.rpfcoding.borutocharacterviewer.domain.model.OnBoardingPage
-import com.rpfcoding.borutocharacterviewer.ui.theme.welcomeScreenBackgroundColor
+import com.rpfcoding.borutocharacterviewer.ui.theme.*
 
 @ExperimentalPagerApi
 @Composable
@@ -34,13 +42,93 @@ fun WelcomeScreen(navController: NavHostController) {
             count = pages.size,
             state = pagerState,
             verticalAlignment = Alignment.Top
-        ) { page ->
-            PagerScreen(onBoardingPage = pages[page])
+        ) { position ->
+            PagerScreen(onBoardingPage = pages[position])
         }
     }
 }
 
 @Composable
 fun PagerScreen(onBoardingPage: OnBoardingPage) {
-    
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {
+        Image(
+            modifier = Modifier
+                .fillMaxWidth(0.5f)
+                .fillMaxHeight(0.7f),
+            painter = painterResource(id = onBoardingPage.image),
+            contentDescription = stringResource(R.string.on_boarding_image)
+        )
+        Text(
+            modifier = Modifier
+                .fillMaxWidth(),
+            text = onBoardingPage.title,
+            color = MaterialTheme.colors.titleColor,
+            fontSize = MaterialTheme.typography.h4.fontSize,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = EXTRA_LARGE_PADDING)
+                .padding(top = SMALL_PADDING),
+            text = onBoardingPage.description,
+            color = MaterialTheme.colors.descriptionColor,
+            fontSize = MaterialTheme.typography.subtitle1.fontSize,
+            fontWeight = FontWeight.Medium,
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FirstOnBoardingScreenPreview() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        PagerScreen(onBoardingPage = OnBoardingPage.First)
+    }
+}
+
+@Preview(uiMode = UI_MODE_NIGHT_YES, showBackground = true)
+@Composable
+fun FirstOnBoardingScreenDarkPreview() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        PagerScreen(onBoardingPage = OnBoardingPage.First)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SecondOnBoardingScreenPreview() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        PagerScreen(onBoardingPage = OnBoardingPage.Second)
+    }
+}
+
+@Preview(uiMode = UI_MODE_NIGHT_YES, showBackground = true)
+@Composable
+fun SecondOnBoardingScreenDarkPreview() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        PagerScreen(onBoardingPage = OnBoardingPage.Second)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ThirdOnBoardingScreenPreview() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        PagerScreen(onBoardingPage = OnBoardingPage.Third)
+    }
+}
+
+@Preview(uiMode = UI_MODE_NIGHT_YES, showBackground = true)
+@Composable
+fun ThirdOnBoardingScreenDarkPreview() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        PagerScreen(onBoardingPage = OnBoardingPage.Third)
+    }
 }
