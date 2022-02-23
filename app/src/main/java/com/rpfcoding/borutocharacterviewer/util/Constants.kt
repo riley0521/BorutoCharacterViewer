65f41323717682d6624f1d88e8fc28ac7adbb67a
riley <rileyfarro@gmail.com>
2022-02-23 12:16:24 +0800

    Created BorutoApi interface that have two functions. Created NetworkModule singleton class to instruct dagger hilt how to provide ok http client, retrofit instance, and boruto api implementation. Also the ip address of 'localhost' in android studio.

--- app/src/main/java/com/rpfcoding/borutocharacterviewer/util/Constants.kt
+++ app/src/main/java/com/rpfcoding/borutocharacterviewer/util/Constants.kt
@@ -7,4 +7,6 @@ object Constants {
     const val PREFERENCES_NAME = "boruto_preferences"
     const val PREF_ON_BOARDING_KEY = "on_boarding_completed"
 
+    const val BASE_URL = "http://10.0.2.2:8080/"
+
 }
