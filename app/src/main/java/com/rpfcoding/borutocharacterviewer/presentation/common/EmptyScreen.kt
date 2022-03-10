8a937508bec71db541dbb687142f3b358479ec74
riley <rileyfarro@gmail.com>
2022-03-10 20:25:37 +0800

    Added SwipeRefresh composable in EmptyContent composable to enable refreshing paging items when error is not empty and actually request again for data if needed.

--- app/src/main/java/com/rpfcoding/borutocharacterviewer/presentation/common/EmptyScreen.kt
+++ app/src/main/java/com/rpfcoding/borutocharacterviewer/presentation/common/EmptyScreen.kt
@@ -6,6 +6,8 @@ import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -19,7 +21,11 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.paging.LoadState
+import androidx.paging.compose.LazyPagingItems
+import com.google.accompanist.swiperefresh.SwipeRefresh
+import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 import com.rpfcoding.borutocharacterviewer.R
+import com.rpfcoding.borutocharacterviewer.data.local.entity.HeroEntity
 import com.rpfcoding.borutocharacterviewer.ui.theme.DarkGray
 import com.rpfcoding.borutocharacterviewer.ui.theme.LightGray
 import com.rpfcoding.borutocharacterviewer.ui.theme.NETWORK_ERROR_ICON_HEIGHT
@@ -29,7 +35,8 @@ import java.net.SocketTimeoutException
 
 @Composable
 fun EmptyScreen(
-    error: LoadState.Error? = null
+    error: LoadState.Error? = null,
+    heroes: LazyPagingItems<HeroEntity>? = null
 ) {
     var message by remember {
         mutableStateOf("Find your favorite Hero!")
@@ -59,7 +66,9 @@ fun EmptyScreen(
     EmptyContent(
         alphaAnim = alphaAnim,
         icon = icon,
-        message = message
+        message = message,
+        error = error,
+        heroes = heroes
     )
 }
 
@@ -67,31 +76,51 @@ fun EmptyScreen(
 fun EmptyContent(
     alphaAnim: Float = 1f,
     @DrawableRes icon: Int = R.drawable.ic_network_error,
-    message: String
+    message: String,
+    error: LoadState.Error? = null,
+    heroes: LazyPagingItems<HeroEntity>? = null
 ) {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+
+    var isRefreshing by remember {
+        mutableStateOf(false)
+    }
+
+    SwipeRefresh(
+        swipeEnabled = error != null,
+        state = rememberSwipeRefreshState(isRefreshing = isRefreshing),
+        onRefresh = {
+            isRefreshing = true
+            heroes?.refresh()
+            isRefreshing = false
+        }
     ) {
-        Icon(
+        Column(
             modifier = Modifier
-                .size(size = NETWORK_ERROR_ICON_HEIGHT)
-                .alpha(alpha = alphaAnim),
-            painter = painterResource(id = icon),
-            contentDescription = stringResource(id = R.string.network_error_icon),
-            tint = if(isSystemInDarkTheme()) LightGray else DarkGray
-        )
-        Text(
-            modifier = Modifier
-                .padding(top = SMALL_PADDING)
-                .alpha(alpha = alphaAnim),
-            text = message,
-            color = if (isSystemInDarkTheme()) LightGray else DarkGray,
-            textAlign = TextAlign.Center,
-            fontWeight = FontWeight.Medium,
-            fontSize = MaterialTheme.typography.subtitle1.fontSize
-        )
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState()),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Icon(
+                modifier = Modifier
+                    .size(size = NETWORK_ERROR_ICON_HEIGHT)
+                    .alpha(alpha = alphaAnim),
+                painter = painterResource(id = icon),
+                contentDescription = stringResource(id = R.string.network_error_icon),
+                tint = if(isSystemInDarkTheme()) LightGray else DarkGray
+            )
+            Text(
+                modifier = Modifier
+                    .padding(top = SMALL_PADDING)
+                    .alpha(alpha = alphaAnim),
+                text = message,
+                color = if (isSystemInDarkTheme()) LightGray else DarkGray,
+                textAlign = TextAlign.Center,
+                fontWeight = FontWeight.Medium,
+                fontSize = MaterialTheme.typography.subtitle1.fontSize
+            )
+        }
+
     }
 }
 
